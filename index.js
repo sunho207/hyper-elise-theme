6691e47f57964eda851a772bc45d565f076eba65
Andy Kim <sunho207@andys-mbp.lan>
2019-11-27 22:08:38 -0800

    Updating theme

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const backgroundColor = '#2b303b'
-const borderColor = '#2b303b'
-const foregroundColor = '#c0c5ce'
+const backgroundColor = '#323F4E'
+const borderColor = '#323F4E'
+const headerColor = '#3C4C5F'
+const foregroundColor = '#F8F8F2'
 const cursorColor = '#c0c5ce'
 
 const colors = {
   black: backgroundColor,
   red: '#bf616a',
   green: '#a3be8c',
-  yellow: '#ebcb8b',
-  blue: '#8fa1b3',
-  magenta: '#b48ead',
-  cyan: '#96b5b4',
+  yellow: '#F1FA8C',
+  blue: '#56687E',
+  magenta: '#BD99FF',
+  cyan: '#92B6F4',
   white: foregroundColor,
   lightblack: backgroundColor,
   lightRed: '#bf616a',
   lightGreen: '#a3be8c',
   lightYellow: '#ebcb8b',
-  lightBlue: '#8fa1b3',
+  lightBlue: '#3C4C5F',
   lightMagenta: '#b48ead',
   lightCyan: '#96b5b4',
   lightWhite: foregroundColor
@@ -39,17 +40,24 @@ exports.decorateConfig = config =>
       /* Tab colors */
       .tab_tab {
         color: ${foregroundColor} !important;
-        background-color: ${backgroundColor} !important;
+        background-color: ${headerColor} !important;
         border: none !important;
         border-right: 1px solid transparent !important;
         border-left: 1px solid transparent !important;
         border-bottom: 1px solid transparent !important;
         transition: border-color .4s ease !important;
+        transition: background-color .4s ease !important;
+      }
+
+      /* Header colors */
+      .header_header {
+        background-color: ${headerColor} !important;
       }
 
       /* Tab emphasized when active */
       .tab_active {
-        border-bottom: 1px solid ${colors.blue} !important;
+        background-color: ${backgroundColor} !important;
+        transition: background-color .4s ease !important;
       }
     `
   })
